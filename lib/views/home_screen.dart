2acba20cfeb0cb04f6cfb9895f83ca6345b13870
NADIYA109 <khannadiya109@gmail.com>
2025-07-24 01:21:00 +0530

    add profile screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:online_donation_app/providers/user_provider.dart';
+import 'package:online_donation_app/views/profile_screen.dart';
 import 'package:provider/provider.dart';
 import 'category_details_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
   final List<Map<String, dynamic>> categories = [
     {'title': 'Education', 'icon': Icons.school},
     {'title': 'Healthcare', 'icon': Icons.health_and_safety},
@@ -16,6 +24,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final userProvider = Provider.of<UserProvider>(context);
     final userName = userProvider.user?.displayName ?? "User";
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -30,106 +39,113 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Welcome, $userName!",
-              style: const TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              "Donation Categories",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-                color: Colors.blueGrey,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Expanded(
-              child: GridView.builder(
-                itemCount: categories.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
+
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcome, $userName!",
+                  style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
                 ),
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => CategoryDetailsScreen(category: categories[index]['title']),
+                const SizedBox(height: 20),
+                const Text(
+                  "Donation Categories",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Expanded(
+                  child: GridView.builder(
+                    itemCount: categories.length,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                    ),
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => CategoryDetailsScreen(
+                                category: categories[index]['title'],
+                              ),
+                            ),
+                          );
+                        },
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          elevation: 6,
+                          shadowColor: Colors.blueAccent.withOpacity(0.3),
+                          color: Colors.blue[50],
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(categories[index]['icon'], size: 50, color: Colors.blueAccent),
+                              const SizedBox(height: 10),
+                              Text(
+                                categories[index]['title'],
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black87,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       );
                     },
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      elevation: 6,
-                      shadowColor: Colors.blueAccent.withOpacity(0.3),
-                      color: Colors.blue[50],
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(categories[index]['icon'], size: 50, color: Colors.blueAccent),
-                          const SizedBox(height: 10),
-                          Text(
-                            categories[index]['title'],
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black87,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+
+        
+          const Center(child: Text('Updates Screen')),
+
+          
+          const Center(child: Text('History Screen')),
+
+         
+           ProfileScreen(),
+        ],
       ),
+
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
         selectedItemColor: Colors.blueAccent,
         unselectedItemColor: Colors.grey,
         showUnselectedLabels: true,
         items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            label: 'Updates',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'History',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Updates'),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: 'History'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
         onTap: (index) {
-          print("Selected tab: $index");
+          setState(() {
+            _selectedIndex = index;
+          });
         },
       ),
     );
   }
 }
-
-
---
+++ lib/views/profile_screen.dart
@@ -0,0 +1,79 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:online_donation_app/views/signin_screen.dart';
+
+class ProfileScreen extends StatelessWidget{
+  final User? user = FirebaseAuth.instance.currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        ),
+        body: user == null 
+        ? const Center(
+          child: Text("No uer info")) 
+          : Column(
+            children: [
+              const SizedBox(height: 20),
+              CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Colors.grey[300],
+                  child: Icon(Icons.person, size:50, color: Colors.white),
+                ),
+              const SizedBox(height: 10),
+              Text(
+                user!.displayName ?? "No Name",
+                style: TextStyle(
+                  color: Colors.black ,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(user!.email ?? "No email", 
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+              ),),
+
+              SizedBox(height: 30,),
+              
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("My Profile"),
+                onTap: (){
+
+                },
+              ),
+
+              ListTile(
+                leading: Icon(Icons.privacy_tip),
+                title: Text("Privacy policy"),
+                onTap: () {},
+              ),
+
+              ListTile(
+                leading: Icon(Icons.logout),
+                title: Text("Logout" , style: TextStyle(
+                  color: Colors.redAccent
+                ),),
+
+                onTap: () async{
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.pushAndRemoveUntil(
+                    context, 
+                    MaterialPageRoute(builder: (_) => LoginScreen()), (route) => false);
+                },
+              ),
+              
+              
+            ],
+          )
+        );
+    
+  }
+
+}
